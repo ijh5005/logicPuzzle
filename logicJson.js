40f8bcf28d66ddbd15b4d0f4aedda84a2e95d258
ijh5005 <ijh5005@outlook.com>
2016-11-16 11:01:49 -0500

    Add files via upload

--- logicJson.js
+++ logicJson.js
@@ -720,7 +720,118 @@ $(document).ready( function () {
   	}
   };
   populatePaper();
-//start puzzle from gallery
+
+
+//change colors of letters on homepage
+	var c = 0;
+	var o = 1;
+	var l = 2;
+	var oo = 3;
+	var r = 4;
+	var ex = 5;
+
+	var colors = ["#FD7C7F", "#E6789F", "#FF9D79", "#4DC57E", "#5C64F9", "#A35DA3"];
+
+	var color = function () {
+		$(".c").css("color", colors[c]);
+		c--
+		if ( c === -1 ) { c = 5; }
+	};
+
+	var color2 = function () {
+		$("#o").css("color", colors[o]);
+		o--
+		if ( o === -1 ) { o = 5; }
+	};
+
+	var color3 = function () {
+		$(".l").css("color", colors[l]);
+		l--
+		if ( l === -1 ) { l = 5; }
+	};
+
+	var color4 = function () {
+		$(".o").css("color", colors[oo]);
+		oo--
+		if ( oo === -1 ) { oo = 5; }
+	};
+
+	var color5 = function () {
+		$(".r").css("color", colors[r]);
+		r--
+		if ( r === -1 ) { r = 5; }
+	};
+
+	var color6 = function () {
+		$(".ex").css("color", colors[ex]);
+		ex--
+		if ( ex === -1 ) { ex = 5; }
+	};
+
+	window.setInterval(function(){
+	color();
+	color2();
+	color3();
+	color4();
+	color5();
+	color6();
+	}, 1000);
+//populate the .guessHomeBox with a 5 by 5 grid of .guessBox's
+	var populationGuessHome = function () {
+	    var markup = "<div class='guessBox floatLeft' correct='false'></div";
+	    for ( i = 0; i < 24; i++) {
+	    	markup += "><div class='guessBox floatLeft' correct='false'></div";
+	    }
+	    markup += ">";
+	    $(".guessHomeBox").html(markup);
+	};
+	populationGuessHome();
+//populate the .leftBarHome with a 5 by 5 grid of .guessBox's
+	var populationLeftBarHome = function () {
+	    var markup = "<div class='leftBar'><span></span></div>";
+	    for ( i = 1; i < 6; i++) {
+	    	markup += "<div class='leftBarOption' data='"+i+"'><span></span></div>";
+	    }
+	    $(".leftBarHome").html(markup);
+	};
+	populationLeftBarHome();
+//populate the .topBarHome with a 5 by 5 grid of .guessBox's
+	var populationLeftBarHome = function () {
+		var markup = "<div class='topBar floatLeft'><span></span></div>";
+		for ( i = 1; i < 6; i++) {
+			markup += "<div class='topBarOption floatLeft' data='"+i+"'><span></span></div>";
+		}
+		$(".topBarHome").html(markup);
+	};
+	populationLeftBarHome();
+//toggle correct answer indicator (light color/dark color)
+    $(".guessBox").click(function () {
+		var backgroundColor = $(this).css("background-color");
+		var dark = $("#start").css("background-color");
+		var medium = $("#story").css("background-color");
+
+		if (backgroundColor == "rgb(255, 255, 255)" || backgroundColor == "rgb(187, 187, 187)"){
+			$(this).css("background-color", dark).attr("correct", false);
+		}
+		else if (backgroundColor == dark){
+			$(this).css("background-color", medium).attr("correct", true);;
+		}
+		else if ( $(this).parent().attr("id") == "topMiddleThird" || $(this).parent().attr("id") == "bottomMiddleSecond" ){
+			$(this).css("background-color", "rgb(187, 187, 187)").attr("correct", false);
+		}
+		else {
+			$(this).css("background-color", "rgb(255, 255, 255)").attr("correct", false);
+		}
+	});
+//reset the puzzle to clear the light/dark colors
+	$("#resetButton").click( function () {
+		$(".guessBox").css("background-color", "white");
+		$("#topMiddleThird .guessBox, #bottomMiddleSecond .guessBox").css("background-color", "#BBB");
+	});
+/////////////////////////////////////////////////////////////
+///////////////////////navigation////////////////////////////
+/////////////////////////////////////////////////////////////
+//start the puzzle from gallery
 	$(".puzzleOption").click( function () {
 		var tempHolder, length, j;
 		var location = $(this).attr("data");
@@ -731,15 +842,15 @@ $(document).ready( function () {
 		$("#puzzleGallery").animate({
 			zoom: "50%"
 		}, {duration: 1000, complete: function (){
-			$("body").css("zoom", "130%").css("overflow-y", "hidden");
+			//$("body").css("zoom", "100%").css("overflow-y", "hidden");
 			//hide the #puzzleGallery
 			$("#puzzleGallery").css("display", "none");
 			//bring in the #puzzleContainer
 			$("body").delay(50).animate({
-				zoom: "100%"
-			}, {duration: 800, 
+				//zoom: "100%"
+			}, {duration: 200, 
 				start: function () {
-					$("#puzzleContainer").css("display", "block");
+					$("#puzzleContainer").css("display", "flex");
 					},
 				complete: function (){
 					//set the story paper img
@@ -749,6 +860,49 @@ $(document).ready( function () {
 				});
 		}});
 	});
+//start the puzzle (on start click)
+	$("div#start").click( function () {
+		//$("#win").fadeOut("fast");
+		$("div#story").animate({
+			top: "-=100vh"
+		}, {
+				duration: 1000, 
+				complete: function () {
+							// $("body").css("overflow-y", "hidden");
+							$("#puzzleStoryHolder").css("display", "block");
+			$("#puzzleStoryHolder").animate({
+				top: "-=100vh"
+			}, 1000);
+		}});
+	});  	
+//go back to gallery
+  	$("#story #backButton").click( function () {
+  		$("body").css("overflow-y", "").css("background-color", "#CCC");
+  		//$("#puzzleStory").css("display", "none");
+  		$(".puzzleOption").css("display", "block");
+  		//$(".puzzleOption").attr("id","");
+		$("#puzzleContainer").fadeOut();
+		$("#puzzleGallery").css("zoom", "100%").delay(800).fadeIn();
+		$("#storyPage").css("background-image", "none");
+		$(".guessBox").css("background-color", "white");
+		$("#topMiddleThird .guessBox, #bottomMiddleSecond .guessBox").css("background-color", "#BBB");
+		$("#puzzleStory.puzzle").css("display", "none");
+	});	
+//go to the previous screen
+  	$("#puzzleStory #backButton").click( function () {
+		$("#puzzleStory.puzzle").animate({
+			top: "+=100vh"
+		}, {duration: 1000,  start: function () {
+			$("body").css("overflow-y", "hidden");
+			$("#puzzleStory.puzzle").css("display", "block");
+			$("div#story").animate({
+				top: "+=100vh"
+			}, 1000);
+		}});
+	});
+
+
+
 //markup creator function
 	var markup = function (selector, target){
 		$(selector).html(target);
